c3bafc9506699dbee1b9a8c9a0e7adeebe4f5dfa
unknown <Chris@Eleri.(none)>
2012-01-15 09:48:17 +0000

    Notes and Code from exercises 1.20 and 1.22

---
+++ ch1/sicp-1.22.scm
@@ -0,0 +1,33 @@
+(define (search from)
+  (sfp from (+ 200 from)))
+
+(define (sfp from to)
+  (timed-prime-test from)
+  (if (< from to) (sfp (+ from 1) to) 0))
+
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime n (- (runtime) start-time))))
+
+(define (report-prime n elapsed-time)
+  (newline)
+  (display n)
+  (display " *** ")
+  (display (* 1000 elapsed-time)))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor)
+    (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
